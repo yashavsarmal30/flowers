3fef46f04cbe0ebb7c59e99371e81654ee807aa7
Yash Avsarmal <159022676+avsarmalsagar05@users.noreply.github.com>
2024-04-10 23:36:35 +0530

    update

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
-onload = () =>{
-        document.body.classList.remove("container");
-};
+// Wait for the DOM content to be fully loaded
+document.addEventListener("DOMContentLoaded", function() {
+    // Redirect to index.html
+    window.location.replace("flower.html");
+});
